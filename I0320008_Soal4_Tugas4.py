fc4c14c052dc78b655f7eec13eff8728ed86ad9f
Alifiana Rahma <alifianars03@gmail.com>
2021-03-26 21:24:14 +0700

    alif

---
+++ I0320008_Soal4_Tugas4.py
@@ -0,0 +1,12 @@
+usia = 21
+a = int(input("Berapa Umur Anda : "))
+if usia <= a :
+    print("Anda Cukup Umur")
+   b = str(input("Apakah anda lulus ujian kualifikasi (Y/T)")
+    if b == "Y" :
+        print("Anda dapat mendaftar di kursus")
+    elif b == "T" :
+        print("Anda tidak dapat mendaftar di kursus")
+
+else :
+    print("Anda Belum Cukup Umur")
---
+++ I0320008_exercise4.10.py
@@ -0,0 +1,11 @@
+# string
+
+str = "Hello World"
+
+
+
+# skip
+
+new_str = str[0::2]
+
+print(new_str)
